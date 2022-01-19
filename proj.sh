307b43c16d09e49bf494c205a9e7e1f882ca6a84
Ali-vaezi <avaezi0079@gmail.com>
2022-01-19 05:32:01 -0500

    2nd answer: first echo is title_ awk: for printing the  of the file and pass it to sort and uniq_ uinq: used with -c to count the (similar)lines_ sort -n: numeric sort_sort -r: reverse the sort because the default sort is from less to greater_ head -n 10: give us the first n line_ at the end print the item

--- proj.sh
+++ proj.sh
@@ -11,3 +11,10 @@ for item in `less $file|awk '{print $11}'`; do
 done
 
 echo 1- tedad bazdid haye shahroodut.ac.ir : $c
+
+#-----------
+echo --------'\n'2- 10 maximum ip address
+
+for item in `less $file|awk '{print $1}'|sort|uniq -c|sort -n -r|head -n 10`; do
+	echo $item
+done
